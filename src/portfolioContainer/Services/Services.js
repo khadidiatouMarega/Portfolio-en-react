06784f4928cfa01c164cebd0641f2063042e9c0d
khadidiatou <khadidiatou.marega@epitech.eu>
2021-12-08 12:26:23 +0100

    update la responsivité

--- src/portfolioContainer/Services/Services.js
+++ src/portfolioContainer/Services/Services.js
@@ -18,16 +18,16 @@ export default function Services() {
                 <div className="border"></div>
                 <h1 className="competenceT" id="font_t">Compétences</h1>
                 <div className='logo'>
-                    <div className="symf logo1"><img className="symfony techno_icon" src={Symfony} /></div>
+                    <div className="symf logo1 symfon"><img className="symfony techno_icon" src={Symfony} /></div>
                     <div className="symf logo1"><img className="laravel techno_icon" src={Laravel} /></div>
                     <div className="symf logo2"><img className="js techno_icon" src={JS} /></div>
                     <div className="symf logo2"><img className="react techno_icon" src={REACT} /></div>
                 </div>
-                <div className='logo'>
-                    <div className="symf logo3"><img className="html techno_icon" src={Html} /></div>
-                    <div className="symf logo3"><img className="sql techno_icon" src={SQL} /></div>
-                    <div className="symf logo4"><img className="php techno_icon" src={PHP} /></div>
-                    <div className="symf logo4"><img className="mongo techno_icon" src={Mongo}/></div>
+                <div className='logo logo_b'>
+                    <div className="symf logo3 symfon"><img className="html techno_icon" src={Html} /></div>
+                    <div className="symf logo3 sq"><img className="sql techno_icon" src={SQL} /></div>
+                    <div className="symf logo4 pp"><img className="php techno_icon" src={PHP} /></div>
+                    <div className="symf logo4 "><img className="mongo techno_icon2" src={Mongo}/></div>
                 </div>
             </div>
   );
